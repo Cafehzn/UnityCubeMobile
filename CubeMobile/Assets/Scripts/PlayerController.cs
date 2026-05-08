b67e452f1cc570f16f42a089a8dc70df12d7a705
JoopGamez <jpb5534@gmail.com>
2026-05-07 21:55:19 -0300

    Update

---
+++ CubeMobile/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector2 movementInput;
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float maxSpeed = 20f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnMove(InputValue value)
+    {
+        movementInput = value.Get<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = 
+            new Vector3(movementInput.x, 0f, movementInput.y) * speed;
+
+        if(rb.linearVelocity.magnitude < maxSpeed)
+        {
+            rb.linearVelocity =
+            new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
+        }
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
