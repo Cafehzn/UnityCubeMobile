67353c04ecf170c9a35fc0b1ea7750b66f6c24fe
Samuelxxss <sstadm715@gmail.com>
2026-05-08 20:32:40 -0300

    new

--- CubeMobile/Assets/Scripts/GameManager.cs
+++ CubeMobile/Assets/Scripts/GameManager.cs
@@ -9,12 +9,28 @@ public class GameManager : MonoBehaviour
     public float spawnY = 12f;
     public float spawnX = 8.5f;
 
+    private void Start()
+    {
+        StartCoroutine(SpawnObstacle());
+    }
+
     private IEnumerator SpawnObstacle()
     {
         while (!isGameOver)
         {
-            var xPosition = Random.Range(-spawnX, spawnX);
-            Instantiate(obstaclePrefab, new Vector3(xPosition, spawnY, 0), Quaternion.identity);
+            var obstacleSpawn = Random.Range(1, 4);
+
+            for(int i = 0; i < obstacleSpawn; i++)
+            {
+                var xPosition = Random.Range(-spawnX, spawnX);
+
+                var damping = Random.Range(0f, 2f); //Air resistence
+
+                var objObstacle =
+                    Instantiate(obstaclePrefab, new Vector3(xPosition, spawnY, 0), Quaternion.identity);
+
+                objObstacle.GetComponent<Rigidbody>().linearDamping = damping;
+            }
 
             yield return new WaitForSeconds(spawnInterval);
         }
